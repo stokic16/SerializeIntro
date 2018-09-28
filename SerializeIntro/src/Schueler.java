03bcb3d7d863c4dd23945b50bcc6a5d92b88e6fd
Kilian <kiliansc5@gmail.com>
2018-09-28 09:57:14 +0200

    added new class Schueler

---
+++ SerializeIntro/src/Schueler.java
@@ -0,0 +1,35 @@
+
+import java.time.LocalDate;
+
+
+
+/**
+ * @author Kilian Stöckler
+ */
+public class Schueler {
+    private String name;
+    private LocalDate birthday;
+
+    public Schueler(String name, LocalDate birthday) {
+        this.name = name;
+        this.birthday = birthday;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    
+}
